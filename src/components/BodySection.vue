19ba828e7d47335c24a4a290e0f83843acb81f4a
AnwarK.code <115826335+AnwarKXC@users.noreply.github.com>
2023-09-28 09:42:46 +0300

    fix responsive

--- src/components/BodySection.vue
+++ src/components/BodySection.vue
@@ -1,9 +1,9 @@
 <template>
-   <main class=" md:w-2/3  md:mx-auto md:flex md:px-16 px-2">
-      <div class="  md:w-1/2 w-screen ">
+   <main class=" 2xl:w-2/3  xl:mx-auto flex flex-col justify-center items-center lg:flex-row xl:px-16 px-2">
+      <div class="  lg:w-1/2  ">
          <img  src="/public/Rectangle_3-removebg-preview.png" alt="">
       </div>
-      <div class=" w-screen md:py-24 order-3">
+      <div class="  lg:w-1/2 md:py-24 order-3">
          <h1 class=" md:text-4xl text-2xl font-bold border-l-[16px] border-red-700 p-2">HOW WE GOT STARTED IN <br> THIS BUSINESS
          </h1>
          <p class="md:py-12 py-6 pb-16 px-6 max-w-lg font-medium tracking-wide md:leading-7">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est corrupti illo quia amet harum, illum, ullam similique totam voluptate magnam animi veritatis blanditiis, consectetur ratione unde molestias necessitatibus omnis sequi!</p>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
    <header class=" relative w-screen md:h-[375px] h-[350px]  ">
       <nav 
-         class="bg-black w-screen h-28 text-white flex list-none gap-8 items-center sticky z-10 md:justify-end justify-center md:px-24 font-medium px-2 ">
+         class="bg-black w-screen h-28 text-white flex list-none md:gap-8 gap-4 items-center sticky z-10 md:justify-end justify-center md:px-24 font-medium px-2  ">
          <li><a href="">HOME</a></li>
          <li><a href="">ABOUT</a></li>
          <li><a href="">CONTRACT</a></li>
@@ -11,16 +11,16 @@
          </div>
       </nav>
       <h1
-         class="text-white font-bold text-3xl md:text-6xl absolute top-32 md:top-20 tracking-wide md:leading-[60px] leading-[46px] z-10 md:left-24 left-[calc(5%)]">
+         class="text-white font-bold text-3xl lg:text-6xl absolute top-32 lg:top-20 tracking-wide lg:leading-[60px] leading-[46px] z-10 md:left-24 left-[calc(5%)]">
          FIGHT LIKE A <br>
          <span class=" text-red-600">CHAMPION</span>
       </h1>
-      <div class="  z-20  md:-top-52 -top-36 relative md:left-[calc(30%)] md:w-[calc(40%)] scale-90">
-         <div class="left-[calc(85%-196px)] md:left-[calc(50%-196px)] relative md:w-96 w-80 top-14">
+      <div class="  z-20  lg:-top-52 -top-36 relative lg:left-[calc(30%)] lg:w-[calc(40%)] scale-90">
+         <div class="left-[calc(85%-196px)] lg:left-[calc(70%-196px)] lg:top-[105px] relative xl:w-96 w-80 xl:left-[calc(50%-196px)] xl:top-10  top-14">
             <img src="/public/05b8ff275ddaafa20e243c159c3114a4-removebg-preview.png" alt="">
          </div>
          <div
-            class="  bg-[#e6e6e6] md:top-[360px] top-[330px] z-30 absolute rounded-xl w-full md:h-24  py-1 drop-shadow-2xl shadow flex justify-around  items-center text-red-700 font-semibold md:text-[22px] ">
+            class=" md:min-w-[600px] bg-[#e6e6e6] md:top-[360px] top-[330px] z-30 absolute rounded-xl w-full md:h-24  py-1 drop-shadow-2xl shadow flex justify-around  items-center text-red-700 font-semibold md:text-[22px] ">
 
             <div class="flex flex-col justify-center items-center">
                <span class="material-symbols-outlined text-black md:text-[52px] text-[36px]">
